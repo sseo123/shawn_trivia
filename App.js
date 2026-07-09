d4aaa4e2a0a829d6ab0778ea0d9f0220070de0f1
Shawn Seo <sseo2@Shawns-MacBook-Pro-2.local>
2026-07-09 15:03:32 -0700

    completed step 3/4

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { useState, useCallback, useEffect } from "react";
 import { GiftedChat } from "react-native-gifted-chat";
-import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 
 const CHATBOT_USER_OBJ = {
   _id: 2,
@@ -8,9 +8,16 @@ const CHATBOT_USER_OBJ = {
   avatar: "https://loremflickr.com/140/140",
 };
 
+const game_db = [
+  { question: "1 + 1", answer: "2" },
+  { question: "2 + 2", answer: "4" },
+  { question: "3 + 3", answer: "6" },
+];
+
 export default function App() {
   const [messages, setMessages] = useState([]);
-
+  const [play, setPlay] = useState(false);
+  const [gameIndex, setGameIndex] = useState(0);
 
   useEffect(() => {
     if (messages.length < 1) {
@@ -42,8 +49,54 @@ export default function App() {
 
   const respondToUser = (userMessages) => {
     console.log("Recent user msg:", userMessages[0].text);
+    if (userMessages[0].text.toLowerCase() == "yes") {
+      startGame();
+    } else {
+      addBotMessage(
+        "Hello, welcome to simple trivia! Say 'Yes' when you're ready to play!",
+      );
+    }
+  };
 
-    // addBotMessage("I am da response!");
+  const startGame = () => {
+    setPlay(true);
+    setGameIndex(0);
+    addBotMessage(" Press 'r' to restart");
+    addBotMessage(`What is ${game_db[0].question}`);
+  };
+
+  // this can be the game logic
+  const game_responce = (userMessages) => {
+    console.log("Recent user msg:", userMessages[0].text);
+
+    let my_bool = true;
+
+    const curr_problem = game_db[gameIndex];
+    console.log(curr_problem.answer);
+
+    if (userMessages[0].text == curr_problem.answer) {
+      addBotMessage("Correct! Press 'r' to restart");
+    } else if (userMessages[0].text.toLowerCase() == "r") {
+      setPlay(false);
+      addBotMessage("Thank you for playing");
+      addBotMessage("Say 'yes' to play again.");
+      my_bool = false;
+    } else {
+      addBotMessage("Incorrect. Press 'r' to restart");
+    }
+
+    if (my_bool) {
+      if (gameIndex + 1 <= 2) {
+        // let nextIndex = gameIndex + 1;
+        setGameIndex(gameIndex + 1);
+        addBotMessage(
+          `Next question: What is ${game_db[gameIndex + 1].question}?`,
+        );
+      } else {
+        addBotMessage("Game over. Say 'yes' to play again.");
+        setPlay(false);
+      }
+    }
   };
 
   const onSend = useCallback((messages = []) => {
@@ -52,21 +105,37 @@ export default function App() {
 
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={{flex: 1}}>
-        <GiftedChat
-          messages={messages}
-          onSend={(messages) => {
-            onSend(messages);
-            // Wait a sec before responding
-            setTimeout(() => respondToUser(messages), 1000);
-          }}
-          user={{
-            _id: 1,
-            name: "Chilla",
-          }}
-          renderUsernameOnMessage={true}
-        />
-      </SafeAreaView> 
+      <SafeAreaView style={{ flex: 1 }}>
+        {play ? (
+          <GiftedChat
+            messages={messages}
+            onSend={(messages) => {
+              onSend(messages);
+              // Wait a sec before responding
+              setTimeout(() => game_responce(messages), 1000);
+            }}
+            user={{
+              _id: 1,
+              name: "Chilla",
+            }}
+            renderUsernameOnMessage={true}
+          />
+        ) : (
+          <GiftedChat
+            messages={messages}
+            onSend={(messages) => {
+              onSend(messages);
+              // Wait a sec before responding
+              setTimeout(() => respondToUser(messages), 1000);
+            }}
+            user={{
+              _id: 1,
+              name: "Chilla",
+            }}
+            renderUsernameOnMessage={true}
+          />
+        )}
+      </SafeAreaView>
     </SafeAreaProvider>
   );
 }
